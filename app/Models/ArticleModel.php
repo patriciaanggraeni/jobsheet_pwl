393141eee0e794bee17ee56ec692293ea2b23e9c
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-05-15 09:20:27 +0700

    Praktikum 10 -> Membuat controller dan model article serta mengisi fillable

---
+++ app/Models/ArticleModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ArticleModel extends Model {
+
+    use HasFactory;
+    protected $fllable = ['title', 'content', 'featured_image'];
+}
